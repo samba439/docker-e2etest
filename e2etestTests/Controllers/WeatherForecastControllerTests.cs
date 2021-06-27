6fd9317d654f68ef09bcb9873cd255e4dd8830b4
Samba <ksamba@innoclique.com>
2021-06-27 22:15:47 +0530

    Add project files.

---
+++ e2etestTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using e2etest.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace e2etest.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
